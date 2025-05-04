9e73ec52641b1b0a5ce57598656394e71b30d521
YanatcharaChatnarin <150699179+YanatcharaChatnarin@users.noreply.github.com>
2025-05-04 14:56:35 +0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from ultralytics import YOLO
+import tempfile
+import os
+
+# โหลดโมเดล (ใช้ path สั้น เช่น best.pt)
+model = YOLO("/content/runs/detect/yolov8n_muffin_chihuahua5/weights/best.pt")
+
+def detect_muffin_or_chihuahua(image: Image.Image):
+    # บันทึกภาพชั่วคราว
+    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_file:
+        image.save(temp_file.name)
+        temp_path = temp_file.name
+
+    # ทำนาย
+    results = model.predict(source=temp_path, conf=0.3, save=False)
+    result_img = results[0].plot()
+    result_pil = Image.fromarray(result_img)
+    labels = [model.names[int(cls)] for cls in results[0].boxes.cls]
+
+    os.remove(temp_path)
+    return labels, result_pil
