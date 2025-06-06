38a751d7c077164d41e5fd15cb826dbdac417984
Yanatcharita <150699179+Yanatcharita@users.noreply.github.com>
2025-06-06 14:31:10 +0000

    edit requirements.txt

--- model.py
+++ model.py
@@ -9,23 +9,23 @@ import numpy as np
 model = YOLO("best.pt")
 
 def detect_muffin_or_chihuahua(image: Image.Image):
-    # save image temporarily
+    # save images
     with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_file:
         image.save(temp_file.name)
         temp_path = temp_file.name
 
     # predict
     results = model.predict(source=temp_path, conf=0.5, save=False)
-    result_img = results[0].plot()  # BGR NumPy array from YOLO
+    result_img = results[0].plot()
 
-    # Convert BGR to RGB for displaying
-    result_rgb = cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB)
+    # แปลงจาก BGR เป็น RGB (ผลลัพธ์จาก .plot() เป็น BGR)
+    result_bgr = cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR)
+    
+    # แปลงกลับเป็น RGB ก่อนแสดงผล
+    result_rgb = cv2.cvtColor(result_bgr, cv2.COLOR_BGR2RGB)
+    
     result_pil = Image.fromarray(result_rgb)
-
-    # Get detected labels
     labels = [model.names[int(cls)] for cls in results[0].boxes.cls]
 
-    # Clean up
     os.remove(temp_path)
-
     return labels, result_pil
